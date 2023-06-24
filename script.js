32a5b14fb96a296dc69087d3e2ecbb3d3581186d
eddyK15501 <itgma1231@gmail.com>
2023-06-23 23:43:52 -0400

    script.js completed. comment clean up

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ $(function () {
   // set the color of the time block 
   const setColor = () => {
     $('.time-block').each(function() {
-      // grab the id of each time block 
       const timeBlock = this.id;
 
       // if the id matches the current hour, then set color of the timeblock accordingly
@@ -25,16 +24,13 @@ $(function () {
     const date = dayjs().format('ddd. MMMM D, YYYY');
     const time = dayjs().format('hh:mm:ss A');
 
-    // update innerText using template literals
     currentDay.text(`${date} | ${time}`);
   }
 
   // save notes to localStorage, when clicking the save button
   const saveDate = () => {
     $('.btn').on('click', function () {
-      // set localStorage key as the id of each time block 
       const key = $(this).parent().attr('id');
-      // set localStorage value as the string written in the textarea
       const value = $(this).siblings('textarea').val();
 
       localStorage.setItem(key, value);
