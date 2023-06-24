cd234142ea4463c63731cf829b17bc72d665b12e
eddyK15501 <itgma1231@gmail.com>
2023-06-23 23:41:22 -0400

    script.js completed

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   // get the current hour in 24-hour clock time using day.js
   const hour = dayjs().format('HH');
