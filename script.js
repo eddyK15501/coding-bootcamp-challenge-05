b2c041ffc9cdb15d667de8e81b54265d9992872b
eddyK15501 <itgma1231@gmail.com>
2023-06-23 23:02:07 -0400

    script.js completed

--- script.js
+++ script.js
@@ -1,64 +1,55 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
 
   const hour = dayjs().format('HH');
 
   const setColor = () => {
     $('.time-block').each(function() {
-      const timeBlock = this.id
+      const timeBlock = this.id;
 
       if (timeBlock === hour) {
-        $(this).removeClass('past future').addClass('present')
+        $(this).removeClass('past future').addClass('present');
       } else if (timeBlock > hour) {
-        $(this).removeClass('past present').addClass('future')
+        $(this).removeClass('past present').addClass('future');
       } else {
-        $(this).removeClass('present future').addClass('past')
+        $(this).removeClass('present future').addClass('past');
       }
     })
   }
 
-  const setDate = () => {
-    const updateHeader = $('#currentDay')
-    const date = dayjs().format('ddd. MMMM D, YYYY')
-    const time = dayjs().format('hh:mm:ss A')
+  const updateHeader = () => {
+    const currentDay = $('#currentDay');
+    const date = dayjs().format('ddd. MMMM D, YYYY');
+    const time = dayjs().format('hh:mm:ss A');
 
-    updateHeader.text(`${date} | ${time}`)
+    currentDay.text(`${date} | ${time}`);
+  }
+
+  const saveDate = () => {
+    $('.btn').on('click', function () {
+      const key = $(this).parent().attr('id');
+      const value = $(this).siblings('textarea').val();
+
+      localStorage.setItem(key, value);
+    })
+  }
+
+  const getDate = () => {
+    $('.time-block').each(function () {
+      const key = $(this).attr('id');
+      const value = localStorage.getItem(key)
+
+      $(this).children('textarea').val(value)
+    })
   }
 
 
 
-
-
-
-
   setInterval(setColor, 1000)
-  setInterval(setDate, 1000)
-
+  setInterval(updateHeader, 1000)
+  saveDate()
+  getDate()
 });
-
-
-
-
-
-
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page.
